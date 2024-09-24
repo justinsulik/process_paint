a3d4ed4c381deacb5a7518e447bf5ec994d82ab6
Justin Sulik <justin.sulik@gmail.com>
2024-09-24 17:29:16 +0200

    latest version after re-processing covid data

--- process_batch.py
+++ process_batch.py
@@ -4,60 +4,126 @@
 import json, numpy as np, pandas as pd, os, re
 from PIL import Image, ImageOps
 from process_jspaint_data import process_paint
+from collections import defaultdict
 
-# data_in should point to the directory containing the json files
-data_in = "data_in/"
-# this should reflect the size of the sketch in the experiment
-size = 480
+# data_in should point to the directory containing the json files; images_out is folder where images should be stored
+data_in = "data_in_covid"
+data_out = "data_out_covid"
+images_out = "images_out_covid"
+
+# this should reflect the size of the sketch in the experiment...
+# in phase1 (whole body) it was 480x480
+# but in phase 4 (half body) the single-side mask was 368*613, so if width was 480 then height was 480*613/(2*368) = 399.78
+sketch_sizes = {
+    'body': {'width': 480, 'height': 480},
+    'bodyhalf': {'width': 480, 'height': 400},
+    'bodyhalf_single': {'width': 480, 'height': 480}
+}
 
 # Set up masks for body outlines
-whole_body_mask = Image.open("masks/body_mask_double.png").resize((size,size))
+whole_body_mask = Image.open("masks/body/mask_double.png").resize((sketch_sizes['body']['width'], sketch_sizes['body']['height']))
+half_body_mask = Image.open("masks/bodyhalf/mask.png").resize((int(sketch_sizes['bodyhalf']['width']/2), sketch_sizes['bodyhalf']['height']))
+half_body_mask_single = Image.open("masks/bodyhalf_single/mask.png").resize((sketch_sizes['bodyhalf_single']['width'], sketch_sizes['bodyhalf_single']['height']))
 
 # Set up masks for aggregating by zones.
-parts = []
-for file in os.listdir("masks"):
-    if 'body_' in file and 'mask' not in file and 'image' not in file:
-        part_found = re.search('([a-zA-Z]*)\.png', file)
-        if part_found:
-           part = part_found.group(1)
-           parts.append(part)
-## first, create a dict for the mask images to go into
-aggregate_masks = {}
-for part in parts:
-    # load the individual masks and convergt to greyscale
-    part_img = Image.open('masks/body_{}.png'.format(part)).resize((int(size/2),size)).convert('RGB')
-    part_grey = ImageOps.grayscale(part_img)
-    # convert to numpy matrix and change zeroes to NaN
-    part_mat = np.array(part_grey).astype('float')
-    part_mat[part_mat == 0] = np.nan
-    part_mat[part_mat == 255] = 1
-    aggregate_masks[part] = part_mat
+# Each of the above (whole body / half body) could have a separate set of masks
+parts = {}
+for maskset in os.listdir("masks/"):
+    if '.' not in maskset:
+        parts[maskset] = []
+        for file in os.listdir("masks/" + maskset + "/"):
+            if 'body_' in file and 'mask' not in file and 'map' not in file:
+                part_found = re.search('([a-zA-Z]*)\.png', file)
+                if part_found:
+                    part = part_found.group(1)
+                    parts[maskset].append(part)
 
-# for storing the aggregate data
-data_dict = {}
+# first, create a dict for the mask images to go into
+bodypart_masks = {}
+for maskset in parts:
+    bodypart_masks[maskset] = defaultdict(dict)
+    for part in parts[maskset]:
+        if maskset == 'bodyhalf_single':
+            mask_width = int(sketch_sizes[maskset]['width'])
+        else:
+            mask_width = int(sketch_sizes[maskset]['width']/2)
+        mask_height = sketch_sizes[maskset]['height']
+        # load the individual masks and convert to greyscale        
+        part_img = Image.open('masks/{}/body_{}.png'.format(maskset, part)).resize((mask_width, mask_height)).convert('RGB')
+        part_grey = ImageOps.grayscale(part_img)
+        # convert to numpy matrix and change zeroes to NaN
+        part_mat = np.array(part_grey).astype('float')
+        ## The full and half masks seem to be opposites (in terms of mask v background)
+        if maskset == 'body':
+            part_mat[part_mat == 0] = np.nan
+            part_mat[part_mat == 255] = 1
+        else:
+            part_mat[part_mat == 0] = 1
+            part_mat[part_mat == 255] = np.nan
+        bodypart_masks[maskset][part] = part_mat
+
 
 # iterate over json files
 for file_in in os.listdir(data_in):
     if 'json' in file_in:
-        with open(data_in+file_in, "r") as data_file:
-            for data_json in data_file:
-                data = json.loads(data_json)
-                for trial in data:
-                    if trial['trial_type'] == 'paint':
-                        participant_id = trial['participant_id']
-                        strokes_json = trial['strokes']
-                        # create a unique name for this picture
-                        # this can incorporate trial-level data (e.g., what the picture is)
-                        # participant-level data (e.g., their id/demographics)
-                        # study-level data
-                        # or file-system data (e.g., path to a folder).
-                        # But as there is no such data in the test file, I'm just using participant_id as the unique name
-                        aggregate_data = process_paint(strokes_json, participant_id, whole_body_mask, aggregate_masks=aggregate_masks)
-                        # store the aggregate data across participants
-                        data_dict[participant_id] = aggregate_data
-                        # do something sensible with the aggregate_data for each image, e.g. add to csv
+        if re.search('[0-9].json', file_in):
+            # for storing the aggregate data
+            data_dict = {}
 
-# Transform the data into a dataframe
-data_df = pd.DataFrame.from_dict(data_dict, orient="index")
-# Save it
-data_df.to_csv("data_out/data.csv")
+            phase = re.search('([0-9]).json', file_in).group(1)
+            if phase == '2':
+                image_out_path = f"{images_out}/phase{phase}/"
+                if not os.path.exists(image_out_path):
+                    os.makedirs(image_out_path)
+                if not os.path.exists(image_out_path+'png/'):
+                    os.makedirs(image_out_path+'png/')
+                if not os.path.exists(image_out_path+'jpg/'):
+                    os.makedirs(image_out_path+'jpg/')
+                if phase == '1':
+                    mask = whole_body_mask
+                    aggregate_masks = bodypart_masks['body']
+                    image_size = sketch_sizes['body']
+                    double_mask = False
+                    double_aggr = True
+                elif phase == '2':
+                    mask = half_body_mask_single
+                    aggregate_masks = bodypart_masks['bodyhalf_single']
+                    image_size = sketch_sizes['bodyhalf_single']
+                    double_mask = False
+                    double_aggr = False
+                else:
+                    mask = half_body_mask
+                    aggregate_masks = bodypart_masks['bodyhalf']
+                    image_size = sketch_sizes['bodyhalf']
+                    double_mask = True
+                    double_aggr = True
+                with open(f"{data_in}/{file_in}", "r") as data_file:
+                    data = json.load(data_file)
+                    for trial in data:
+                        if trial['trial_type'] == 'paint':
+                            participant_id = trial['participant']
+                            relationship = trial['relationship']
+                            strokes_json = trial['strokes']
+                            # create a unique name for this picture
+                            # this can incorporate trial-level data (e.g., what the picture is)
+                            # participant-level data (e.g., their id/demographics)
+                            # study-level data
+                            # or file-system data (e.g., path to a folder).
+                            # But as there is no such data in the test file, I'm just using participant_id as the unique name
+                            image_id = '{}_{}'.format(participant_id, relationship)
+                            aggregate_data = process_paint(strokes_json, image_id, mask, 
+                                                            out_width = image_size['width'], out_height = image_size['height'], 
+                                                            aggregate_masks=aggregate_masks, 
+                                                            double_mask = double_mask, 
+                                                            double_aggr=double_aggr,
+                                                            save_image_path=image_out_path)
+        
+                            # store the aggregate data across participants
+                            data_dict[(participant_id, relationship)] = aggregate_data
+                        
+                # Transform the data into a dataframe
+                if len(data_dict) > 0:
+                    
+                    data_df = pd.DataFrame.from_dict(data_dict, orient="index").reset_index(names=['participant', 'relationship'])
+                    # Save it
+                    data_df.to_csv(f"{data_out}/phase{phase}_topology_aggr.csv", index=False)
--- process_jspaint_data.py
+++ process_jspaint_data.py
@@ -15,51 +15,60 @@
 # height - height of the drawing canvas
 # stroke - list of [x, y] positions
 
-import json
-import drawSvg as draw
+from ctypes.macholib import dyld
+dyld.DEFAULT_LIBRARY_FALLBACK.append("/opt/homebrew/lib")
+
+import json, os
+import drawsvg as draw
 import numpy as np
 from PIL import Image, ImageOps
 
 def process_paint(strokes_json, file_id,
-    whole_mask, aggregate_masks = None,
+    main_mask, aggregate_masks = None,
     out_width = 480, out_height = 480,
-    render_jpg=True, flip_y=True):
+    render_jpg=True, flip_y=False,
+    double_mask=False,
+    double_aggr=True,
+    save_image_path = 'images_out'):
     # strokes_json: the value of the strokes parameter from a jspsych-paint-d3.js plugin (as json)
     # file_id: unique identifier for this painting trial
+    # main_mask: the mask used for the whole image (to make it appear as in the experiment interface)
     # out_width/height: int size of file output
     # save_png: if true, save a png file from the svg data
     # render_jpg: if true, render+save a jpg file
     # aggregate_masks: optionally, a dict with (string) body part names as keys masking images as values - only use if render_jpg == True
     # flip_y: it seems the gorilla data is flipped about the y-axis. If True, the output will be flipped horizontally
+    # double_mask: if the mask for the whole drawing has already been doubled (to represent front+back) then it can just be applied; otherwise it needs to be applied twice
+    # double_aggr: if the mask for the body-part aggregations has already been doubled (as above...)
+    # save_image_path: the path where the images will be saved
 
     # this function calls subsequent functions to:
     # convert this json string into a dict
     # render this raw stroke data as a png (if save_png is true)
     # and convert it to a jpg (if render_jpg is true)
     # and aggregate by body part (if a mask file is provided in masks)
-
     strokes = json.loads(strokes_json)
 
     # all strokes should be on the same size canvas, so just get the canvas width, height from the first stroke
     if len(strokes) > 0:
         width = strokes[0]['width']
         height = strokes[0]['height']
-        # check if the image is front+back (in which case it will be square; otherwise height=width*2)
-        double_wide = True if height == width else False
-        render_png(strokes, file_id, width, height, out_width, out_height, flip_y)
+        render_png(strokes, file_id, width, height, out_width, out_height, flip_y, save_image_path)
         if render_jpg:
-            image = convert_jpg(file_id, out_width, out_height, whole_mask)
-            if aggregate_masks:
-                aggregate_data = aggregate_zones(image, aggregate_masks, double_wide, width, height)
+            image = convert_jpg(file_id, main_mask, out_width, double_mask, save_image_path)
+            if aggregate_masks and len(aggregate_masks) > 0:
+                aggregate_data = aggregate_zones(image, aggregate_masks, double_aggr, out_width)
                 return aggregate_data
     else:
-        print("There no paint data for ID {}".format(file_id))
+        #print("There no paint data for ID {}".format(file_id))
+        aggregate_data = blank_data(aggregate_masks, double_aggr)
+        return aggregate_data
 
-def aggregate_zones(image, masks, double_wide, width, height):
+def aggregate_zones(image, masks, double_aggr, width):
     # we really only need the blue channel here
     image_mat = np.array(image)[:,:,2]
     aggregate_data = {}
-    if double_wide:
+    if double_aggr:
         # if it is double_wide, separate image into front and back halves,
         halfway = int(width/2)
         sides = {'front': image_mat[:,:halfway], 'back': image_mat[:,halfway:]}
@@ -80,30 +89,51 @@ def stamp_mask(image, mask):
     average = round(np.nanmean(stamped)/255, 2)
     return average
 
-def convert_jpg(file_id, out_width, out_height, whole_mask):
-    raw = Image.open("images_out/png/{}.png".format(file_id))
-    raw.paste(whole_mask, (0,0), whole_mask) #second 'mask' needed to use the transparency
-    rgb_masked = raw.convert('RGB')
-    rgb_masked.save("images_out/jpg/{}.jpg".format(file_id), quality=95, subsampling=0)
+def convert_jpg(file_id, mask, width, double_mask, save_image_path):
+    # double_mask: does the mask need to be appled twice, for front and back?
+    filepathname = f"{save_image_path}/jpg/{file_id}.jpg"
+    if not os.path.isfile(filepathname): 
+        raw = Image.open("{}/png/{}.png".format(save_image_path, file_id))
+        raw.paste(mask, (0,0), mask) #second 'mask' needed to use the transparency; see mask_im example at https://note.nkmk.me/en/python-pillow-paste/ 
+        if double_mask:
+            half_width = int(width/2)
+            raw.paste(mask, (half_width, 0), mask) 
+        rgb_masked = raw.convert('RGB')
+        rgb_masked.save("{}/jpg/{}.jpg".format(save_image_path, file_id), quality=95, subsampling=0)
+    else:
+        rgb_masked = Image.open(filepathname)
     return rgb_masked
 
-def render_png(strokes, file_id, width, height, out_width, out_height, flip_y):
-    d = draw.Drawing(width, height, displayInline=False)
-    # for gaps in the drawing, can fill in the following be fill=NA?
-    d.append(draw.Rectangle(0,0,width,height, fill='#808080'))
-    for brushstroke in strokes:
-        if flip_y:
-            stroke_path = [[point[0], height-point[1]] for point in brushstroke['stroke']]
+def render_png(strokes, file_id, width, height, out_width, out_height, flip_y, save_image_path):
+    filepathname = f"{save_image_path}/png/{file_id}.png"
+    if not os.path.isfile(filepathname):
+        d = draw.Drawing(width, height, displayInline=False)
+        # for gaps in the drawing, can fill in the following be fill=NA?
+        d.append(draw.Rectangle(0,0,width,height, fill='#808080'))
+        for brushstroke in strokes:
+            if flip_y:
+                stroke_path = [[point[0], height-point[1]] for point in brushstroke['stroke']]
+            else:
+                stroke_path = brushstroke['stroke']
+            points = [item for sublist in stroke_path for item in sublist]
+            d.append(draw.Lines(*points,
+                close=False,
+                fill=None,
+                fill_opacity=0,
+                stroke=brushstroke['color'],
+                stroke_width=brushstroke['size'],
+                stroke_linejoin='round',
+                stroke_linecap='round'))
+        d.set_render_size(out_width, out_height)
+        d.save_png(filepathname)
+
+def blank_data(masks, double_wide):
+    #if there is no stroke data, return an appropriate dict with null values
+    data_dict = {}
+    for part in masks:
+        if double_wide:
+            data_dict['{}_front'.format(part)] = 'NA'
+            data_dict['{}_back'.format(part)] = 'NA'
         else:
-            strok_path = brushstroke['stroke']
-        points = [item for sublist in stroke_path for item in sublist]
-        d.append(draw.Lines(*points,
-            close=False,
-            fill=None,
-            fill_opacity=0,
-            stroke=brushstroke['color'],
-            stroke_width=brushstroke['size'],
-            stroke_linejoin='round',
-            stroke_linecap='round'))
-    d.setRenderSize(out_width, out_height)
-    d.savePng('images_out/png/{}.png'.format(file_id))
+            data_dict[part] = 'NA'
+    return data_dict
